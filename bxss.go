48f685a8c216646d5d5840cb06dbb9df079029c4
Jaikishan Tulswani <30144126+jaikishantulswani@users.noreply.github.com>
2024-09-10 12:43:33 +0530

    Update bxss.go
    also add -payloadFile to take multiple payload from file

--- bxss.go
+++ bxss.go
@@ -1,3 +1,20 @@
+Usage of ./bxss:
+  -appendMode
+    	Append the payload to the parameter
+  -concurrency int
+    	Set the concurrency (default 30)
+  -header string
+    	Set a single custom header
+  -headerFile string
+    	Path to file containing headers to test
+  -parameters
+    	Test the parameters for blind xss
+  -payload string
+    	The blind XSS payload
+  -payloadFile string
+    	Path to file containing payloads to test
+                                                                                                                                                                         
+root  bxss   ( master)  ♥ 12:42  cat bxss.go                                                                                                  default@us-west-2
 package main
 
 import (
@@ -26,6 +43,7 @@ func main() {
 	// Flag variables
 	var c int
 	var p string
+	var pf string
 	var h string
 	var hf string
 	var a bool
@@ -35,7 +53,8 @@ func main() {
 	flag.IntVar(&c, "concurrency", 30, "Set the concurrency")
 	flag.StringVar(&h, "header", "", "Set a single custom header")
 	flag.StringVar(&hf, "headerFile", "", "Path to file containing headers to test")
-	flag.StringVar(&p, "payload", "", "the blind XSS payload")
+	flag.StringVar(&p, "payload", "", "The blind XSS payload")
+	flag.StringVar(&pf, "payloadFile", "", "Path to file containing payloads to test")
 	flag.BoolVar(&a, "appendMode", false, "Append the payload to the parameter")
 	flag.BoolVar(&t, "parameters", false, "Test the parameters for blind xss")
 
@@ -54,8 +73,8 @@ func main() {
                     
 	`, "-- Coded by @z0idsec -- \n")
 
-	// Check if the bxss payload is set and either header or headerFile is provided
-	if p == "" || (h == "" && hf == "") {
+	// Check if either payload or payloadFile is provided, and either header or headerFile is provided
+	if (p == "" && pf == "") || (h == "" && hf == "") {
 		flag.PrintDefaults()
 		return
 	}
@@ -63,13 +82,25 @@ func main() {
 	var headers []string
 	if hf != "" {
 		var err error
-		headers, err = readHeadersFromFile(hf)
+		headers, err = readLinesFromFile(hf)
 		if err != nil {
 			fmt.Printf(ErrorColor, "Error reading header file: ", err.Error())
 			return
 		}
 	} else {
 		headers = []string{h}
+	}
+
+	var payloads []string
+	if pf != "" {
+		var err error
+		payloads, err = readLinesFromFile(pf)
+		if err != nil {
+			fmt.Printf(ErrorColor, "Error reading payload file: ", err.Error())
+			return
+		}
+	} else {
+		payloads = []string{p}
 	}
 
 	fmt.Printf(NoticeColor, "\n[-] Please Be Patient for bxss\n ", "")
@@ -79,40 +110,42 @@ func main() {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			processHeaders(headers, p, a, t)
+			processPayloadsAndHeaders(payloads, headers, a, t)
 		}()
 	}
 	wg.Wait()
 }
 
-func readHeadersFromFile(filename string) ([]string, error) {
+func readLinesFromFile(filename string) ([]string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var headers []string
+	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		headers = append(headers, strings.TrimSpace(scanner.Text()))
+		lines = append(lines, strings.TrimSpace(scanner.Text()))
 	}
 
 	if err := scanner.Err(); err != nil {
 		return nil, err
 	}
 
-	return headers, nil
+	return lines, nil
 }
 
-func processHeaders(headers []string, payload string, appendMode bool, isParameters bool) {
+func processPayloadsAndHeaders(payloads []string, headers []string, appendMode bool, isParameters bool) {
 	scanner := bufio.NewScanner(os.Stdin)
 	client := &http.Client{Timeout: 3 * time.Second}
 
 	for scanner.Scan() {
 		link := scanner.Text()
-		for _, header := range headers {
-			testbxss(client, payload, link, header, appendMode, isParameters)
+		for _, payload := range payloads {
+			for _, header := range headers {
+				testbxss(client, payload, link, header, appendMode, isParameters)
+			}
 		}
 	}
 }
